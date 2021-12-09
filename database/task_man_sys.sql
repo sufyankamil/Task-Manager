1efae4cd9b1d9cf524cb085fb879fcb47c71e65f
Sufyan Kamil <sufyankamil15@gmail.com>
2021-12-09 14:44:13 +0530

    sql file updated

--- database/task_man_sys.sql
+++ database/task_man_sys.sql
@@ -2,6 +2,7 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 -- Database : 'task_sys_date'
+----------------------------------------------------------------
 -- Table structure for table 'users'
 CREATE TABLE `users` (
     `id` int(30) NOT NULL,
@@ -10,8 +11,16 @@ CREATE TABLE `users` (
     `username` varchar(200) NOT NULL,
     `password` varchar(400) NOT NULL,
     `email` varchar(200) NOT NULL,
-) ENGINE = InnoDB DEFAULT CHARSET = latin1;
--- Dumping data for table `student`
+) ENGINE = InnoDB DEFAULT CHARSET = utf8_general_ci;
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+ADD PRIMARY KEY (`id`);
+-- AUTO_INCREMENT for table `users`
+ALTER TABLE `users`
+MODIFY `id` int(30) NOT NULL AUTO_INCREMENT,
+    AUTO_INCREMENT = 1;
+-- Dumping data for table `users`
 INSERT INTO `users` (
         `id`,
         `firstname`,
@@ -27,4 +36,48 @@ VALUES (
         'sufyank',
         'jdjsbdjajkasgdkj',
         'sufyan@gmail.com',
+    );
+--- Table structure for table 'task-list'
+CREATE TABLE `task-list`
+) `id` int(30) NOT NULL,
+`task` varchar(200) NOT NULL,
+`date` varchar(200) NOT NULL,
+`description` varchar(200) NOT NULL,
+`status` varchar(200) NOT NULL,
+) ENGINE = InnoDB DEFAULT CHARSET = utf8_general_ci;
+-- Indexes for table `task-list`
+ALTER TABLE `task-list`
+ADD PRIMARY KEY (`id`);
+-- AUTO_INCREMENT for table `task-list`
+ALTER TABLE `task-list`
+MODIFY `id` int(30) NOT NULL AUTO_INCREMENT,
+    AUTO_INCREMENT = 1;
+---- Dumping data for table `task-list`
+INSERT INTO `task-list` (
+        `id`,
+        `task`,
+        `date`,
+        `description`,
+        `status`
+    )
+VALUES (
+        1,
+        'Task 1',
+        '2020-01-01',
+        'Task 1 description',
+        'Completed',
+    );
+INSERT INTO `task-list` (
+        `id`,
+        `task`,
+        `date`,
+        `description`,
+        `status`
+    )
+VALUES (
+        2,
+        'Task 2',
+        '2020-01-01',
+        'Task 2 description',
+        'Completed',
     );
